50954e39a102c874b2c6464223419b5889dbf231
itbeihe-e3 <itbeihe.com>
2015-04-19 12:45:06 +0800

    修改静态路径

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static/',express.static(path.join(__dirname, 'public')));
 
 app.use('/', function(req,res,next){
     //res.redirect('/active/');
